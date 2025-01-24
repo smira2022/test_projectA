d2310d9743d2f4453e40226c807aa1a11bc031c0
smira2022 <sheenapmiranda@yahoo.com>
2025-01-23 19:14:59 -0700

    Update to add userModel

---
+++ src/functions/userModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const  userSchema = new mongoose.Schema({
+    name: {type:String, required: true},
+    email: {type:String, required: true},
+    password: {type:String, required: true},
+    age: {type:Number},
+});
+
+const User = mongoose.model('User', userSchema);
+
+
+const createUser = async (name, email, password, age) =>{
+
+    try {
+        const user = new User({name, email, password, age});
+        await user.save();
+        return user;
+    } catch (error) {
+        throw new Error('Error creating new user');
+    }
+
+}
