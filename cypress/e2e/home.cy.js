ffd9a62302c8f9805da35baeec418b09c80411c3
smira2022 <sheenapmiranda@yahoo.com>
2025-02-12 19:00:42 -0700

    Update add another test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,43 +1,55 @@
 //test suite
-describe('Login Page', ()=>{
-  //pre-condition 
-  beforeEach(()=>{
-    cy.visit('http://localhost:3000');
-  });
+describe('Login Page', () =>{
+  //pre-conditions. alternatively, we can use afterEach for post-conditions
+    beforeEach(() =>{
 
-  //Positive testing
-  it('should login with valid credentials', ()=> {
-
-    //Arrange
-    cy.get('#email').type('test@example.com');
-    cy.get('#password').type('password123');
-
-    //Act
-    cy.get('#login-button').click();
-
-    //Assert
-    cy.on('window:alert', (txt)=> {
-      expect(txt).to.contains('Login Successful!')
+      cy.visit('http://localhost:3000');
     });
 
+
+    //Positive testing
+    it('should login with valid credentials', () =>{
+      //Arrange
+      cy.get('#email').type('test@example.com');
+      cy.get('#password').type('password123');
+
+      //Act
+      cy.get('#login-button').click();
+
+      //Assert
+      cy.on('window:alert', (txt) =>{
+        expect(txt).to.contains('Login Successful!')
+      });
+    });
+
+    //Negative testing
+    it('should display error message if wrong credentials', () =>{
+      //Arrange
+      cy.get('#email').type('test@examples.com');
+      cy.get('#password').type('password123');
+
+      //Act
+      cy.get('#login-button').click();
+
+      //Assert
+      cy.on('window:alert', (txt) =>{
+        expect(txt).to.contains('Invalid credentials')
+      });
   });
 
-  //Negative testing
-  it('should display error message if wrong credentials', ()=> {
-    //Arrange
-    cy.get('#email').type('test@examples.com');
-    cy.get('#password').type('password123');
-
-    //Act
-    cy.get('#login-button').click();
+  //Brute force testing
+  it('should for multiple failed login attempts', () =>{
+ 
+    for(let i = 0; i < 5; i++){
+      cy.get('#email').clear().type('wrongexample@gmail.com')
+      cy.get('#password').clear().type('guessedpassword')
+      cy.get('#login-button').click();
+    }
 
     //Assert
-    cy.get('#error-message').should(
-      'contain',
-      'Invalid email or password'
-    );
+    cy.on('window:alert', (txt) =>{
+      expect(txt).to.contains('Invalid credentials')
+    });
   });
-
-
 
 });
